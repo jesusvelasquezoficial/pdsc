68c40354e8ba9e5c38b61b691855b384caf5072d
Jesus Velasquez <jesusvelasquezoficial@gmail.com>
2019-10-31 10:04:09 -0400

    update links

--- assets/js/assets/auth/index.js
+++ assets/js/assets/auth/index.js
@@ -1,7 +1,7 @@
 const PROTOCOLO = 'http://'
 let IPs = ['localhost', '127.0.0.1', '10.0.1.48'] // 10.0.1.23
 let PORTs = ['4000'] // 8081
-let HOST = 'www.phoenixserver.ml'
+let HOST = 'www.pdsc.ml'
 IPs.forEach(IP => {
   if (location.hostname == IP) {
     HOST = IP
