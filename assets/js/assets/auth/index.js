f427db97016f3b1bc5b6a367033723b0c11df4f2
Jesus Velasquez <jesusvelasquezoficial@gmail.com>
2019-10-31 11:53:46 -0400

    update

--- assets/js/assets/auth/index.js
+++ assets/js/assets/auth/index.js
@@ -1,5 +1,5 @@
 const PROTOCOLO = 'https://'
-let IPs = ['localhost', '127.0.0.1', '10.0.1.48', 'pdsc.ml'] // 10.0.1.23
+let IPs = ['localhost', '127.0.0.1', '10.0.1.48'] // 10.0.1.23
 let PORTs = ['4000'] // 8081
 
 let HOST = 'www.pdsc.ml'
