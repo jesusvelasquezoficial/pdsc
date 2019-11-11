d4c130cdf8a4427008b4493a49f89bdfcbb3fc11
Jesus Velasquez <jesusvelasquezoficial@gmail.com>
2019-11-11 10:08:02 -0400

    .

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -44,7 +44,6 @@ module.exports = (env, options) => ({
               options: {
                 presets: ['@babel/preset-env']
               }
-              
             }
           }
         }
