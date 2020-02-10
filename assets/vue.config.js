975fad9832a7375798fc9636e24408b5ed75aa45
Jesus Velasquez <jesusvelasquezoficial@gmail.com>
2020-02-10 03:05:22 -0400

    up

--- assets/vue.config.js
+++ assets/vue.config.js
@@ -1,3 +1,19 @@
+const path = require('path');
 module.exports = {
-    runtimeCompiler: true
+  runtimeCompiler: true,
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js',
+      'src': path.resolve(__dirname, 'js/'),
+      '@': path.resolve(__dirname, "dist/"),
+      'Components': path.resolve(__dirname, 'dist/components/'),
+      'Constants': path.resolve(__dirname, 'dist/constants/'),
+      'Container': path.resolve(__dirname, 'dist/container/'),
+      'Helpers': path.resolve(__dirname, 'dist/helpers/'),
+      'Assets': path.resolve(__dirname, 'dist/assets/'),
+      'Themes': path.resolve(__dirname, 'dist/themes/'),
+      'Views': path.resolve(__dirname, 'dist/views/'),
+      'lib': path.resolve(__dirname, 'dist/lib/'),
+    }
+  }
 }
