900927ae55a335394289d3fcb78a34c272745d69
Jesus Velasquez <jesusvelasquezoficial@gmail.com>
2020-02-11 12:20:56 -0400

    delete content vue.config.js

--- assets/vue.config.js
+++ assets/vue.config.js
@@ -1,21 +1,3 @@
-const path = require('path');
 module.exports = {
-  runtimeCompiler: true,
-  extensions: ['.js', '.json', '.vue'],
-  modules: [path.join(__dirname, 'src/'), 'node_modules'],
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.common.js',
-      // 'src': path.resolve(__dirname, 'js/'),
-      '@': path.resolve(__dirname, "src/"),
-      'Components': path.resolve(__dirname, 'src/components/'),
-      'Constants': path.resolve(__dirname, 'src/constants/'),
-      'Container': path.resolve(__dirname, 'src/container/'),
-      'Helpers': path.resolve(__dirname, 'src/helpers/'),
-      'Assets': path.resolve(__dirname, 'src/assets/'),
-      'Themes': path.resolve(__dirname, 'src/themes/'),
-      'Pages': path.resolve(__dirname, 'src/pages/'),
-      'lib': path.resolve(__dirname, 'src/lib/'),
-    }
-  }
+  runtimeCompiler: true
 }
